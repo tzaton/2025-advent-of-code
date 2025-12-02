f2a4ce50f2f9e50b7c4350dc6d2bce23eb57c836
tzaton <t.zaton@gmail.com>
2025-12-02 17:45:47 +0100

    first commit

---
+++ day1/solution1.1.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+position = 50
+at_zero = 0
+
+for i, move in enumerate(Path(__file__).parent.joinpath(("input")).read_text().strip().splitlines()):
+    direction = move[0]
+    amount = int(move[1:]) % 100
+
+    print(f"Current position: {position}, Move: {move}")
+
+    if direction == "L":
+        position -= amount
+        if position < 0:
+            position = 100 + position
+    elif direction == "R":
+        position += amount
+        if position > 99:
+            position = position - 100
+    
+    print(f"New position: {position}")
+    if position == 0:
+        at_zero += 1
+    
+print(f"Reached position 0 {at_zero} times")
---
+++ day1/solution1.2.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+position = 50
+at_zero = 0
+
+for i, move in enumerate(Path(__file__).parent.joinpath(("input")).read_text().strip().splitlines()):
+    direction = move[0]
+    amount_total = int(move[1:])
+    amount = int(move[1:]) % 100
+
+    print(f"Current position: {position}, Move: {move}")
+    old_position = position
+
+    if direction == "L":
+        position -= amount
+        if position < 0:
+            position = 100 + position
+        
+        if position > old_position and old_position != 0 and position != 0:
+            at_zero += 1
+
+    elif direction == "R":
+        position += amount
+        if position > 99:
+            position = position - 100
+        
+        if position < old_position and old_position != 0 and position != 0:
+            at_zero += 1
+    
+    print(f"New position: {position}")
+
+    at_zero += amount_total // 100
+
+    if (position == 0) and (amount_total % 100 != 0):
+        at_zero += 1
+
+print(f"Reached position 0 {at_zero} times")
