a2a459ad760dfdd6b8b32e38b8d0a85d1f14f333
tzaton <t.zaton@gmail.com>
2025-12-02 17:46:05 +0100

    day 2

---
+++ day2/solution2.1.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+invalid = []
+
+for num_range in Path(__file__).parent.joinpath("input.txt").read_text().split(","):
+    start, end = num_range.split("-")
+
+    for num in range(int(start), int(end) + 1):
+        print(num)
+
+        num_len = len(str(num))
+        if str(num)[:int(num_len/2)] == str(num)[int(num_len/2):]:
+            print(f"{num} is invalid")
+            invalid.append(num)
+
+print(f"Adding up all the invalid IDs in this example produces: {sum(invalid)}")
---
+++ day2/solution2.2.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+invalid = []
+
+for num_range in Path(__file__).parent.joinpath("input.txt").read_text().split(","):
+    start, end = num_range.split("-")
+
+    for num in range(int(start), int(end) + 1):
+        # print(num)
+
+        num_len = len(str(num))
+
+        for i in range(1, int(num_len / 2) + 1):
+            if num_len % i != 0:
+                continue
+
+            if str(num)[:i] * (num_len // i) == str(num):
+                print(f"{num} is invalid")
+                invalid.append(num)
+                break
+
+print(f"Adding up all the invalid IDs in this example produces: {sum(invalid)}")
